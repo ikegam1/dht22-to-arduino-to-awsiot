ee74afcf481d757b314a914dae65ac5c210d939a
ikegam1 <41247620+ikegam1@users.noreply.github.com>
2019-01-13 13:57:28 +0900

    Update kamekusa.ino
    https://qiita.com/32863806adcb01fe2944/

--- kamekusa.ino
+++ kamekusa.ino
@@ -18,6 +18,12 @@
 #include <Countdown.h>
 #include <MQTTClient.h>
 
+#include <Servo.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <Ambient.h>
+#include <ArduinoJson.h>
+
 //AWS MQTT Websocket
 #include "Client.h"
 #include "AWSWebSocketClient.h"
@@ -27,23 +33,20 @@ extern "C" {
   #include "user_interface.h"
 }
 
-#include <DHT.h>
-#include <DHT_U.h>
-#include <Ambient.h>
-#include <ArduinoJson.h>
-
-#define DHTPIN 4     // what digital pin the DHT22 is conected to
-#define DHTTYPE DHT22   // There are multiple kinds of DHT sensors
+#define DHTPIN 4
+#define DHTTYPE DHT22
 #define JST     3600*9
+#define SERVOPIN 2
 
 //AWS IOT config, change these:
-char wifi_ssid[] = "";
-char wifi_password[] = "";
-char aws_endpoint[] = "xxx-ats.iot.ap-northeast-1.amazonaws.com";
-char aws_key[] = "xxx";
-char aws_secret[] = "xxx";
+char wifi_ssid[] = "xxxx";
+char wifi_password[] = "xxxx";
+char aws_endpoint[] = "xxxx-ats.iot.ap-northeast-1.amazonaws.com";
+char aws_key[] = "xxxx";
+char aws_secret[] = "xxxx";
 char aws_region[] = "ap-northeast-1";
-const char* aws_topic = "xxxx/DHT22";
+const char* aws_topic = "kamekusa/DHT22";
+const char* aws_topic_2 = "kamekusa/SG909G";
 int port = 443;
 
 //MQTT config
@@ -60,7 +63,11 @@ MQTT::Client<IPStack, Countdown, maxMQTTpackageSize, maxMQTTMessageHandlers> cli
 
 //ambient
 DHT dht(DHTPIN, DHTTYPE);
-const int AMBIENT_CHANNEL_ID = 1234;
+Servo kameServo;
+
+int pos = 0;
+
+const int AMBIENT_CHANNEL_ID = 8672;
 const char* AMBIENT_WRITE_KEY = "xxxx";
 
 Ambient ambient;
@@ -74,6 +81,121 @@ char* generateClientID () {
   for (int i=0; i<22; i+=1)
     cID[i]=(char)random(1, 256);
   return cID;
+}
+
+void setup() {
+    kameServo.attach(SERVOPIN);
+    wifi_set_sleep_type(NONE_SLEEP_T);
+    Serial.begin (115200);
+    delay (2000);
+    Serial.setDebugOutput(1);
+
+    //fill with ssid and wifi password
+    WiFiMulti.addAP(wifi_ssid, wifi_password);
+    Serial.println ("connecting to wifi");
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+        Serial.print (".");
+    }
+    Serial.println ("\nconnected");
+
+
+    int timeSinceLastRead = 10;
+    float h;
+    float t;
+    float f;
+    while(timeSinceLastRead > 0) {
+      // Reading temperature or humidity takes about 250 milliseconds!
+      // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+      h = dht.readHumidity();
+      // Read temperature as Celsius (the default)
+      t = dht.readTemperature();
+      // Read temperature as Fahrenheit (isFahrenheit = true)
+      f = dht.readTemperature(true);
+      if(isnan(h) || isnan(t)) {
+        Serial.println("Failed to read from DHT sensor!");
+        timeSinceLastRead -= 1;
+        delay(500);
+      }else{
+        timeSinceLastRead = 0;
+      }
+    }
+
+    // Compute heat index in Fahrenheit (the default)
+    float hif = dht.computeHeatIndex(f, h);
+    // Compute heat index in Celsius (isFahreheit = false)
+    float hic = dht.computeHeatIndex(t, h, false);
+
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print(f);
+    Serial.print(" *F\t");
+    Serial.print("Heat index: ");
+    Serial.print(hic);
+    Serial.print(" *C ");
+    Serial.print(hif);
+    Serial.println(" *F");
+
+    //fill AWS parameters    
+    awsWSclient.setAWSRegion(aws_region);
+    awsWSclient.setAWSDomain(aws_endpoint);
+    awsWSclient.setAWSKeyID(aws_key);
+    awsWSclient.setAWSSecretKey(aws_secret);
+    awsWSclient.setUseSSL(true);
+
+    send_am(h, t, f, hic, hif);
+    delay (100);
+    sendmessage (h,t);
+    Serial.println("Reported!: ");
+  
+    if (connect ()){
+      subscribe ();
+    }
+}
+
+//connects to websocket layer and mqtt layer
+bool connect () {
+
+    if (client.isConnected ()) {    
+        client.disconnect ();
+    }  
+    //delay is not necessary... it just help us to get a "trustful" heap space value
+    delay (1000);
+    Serial.print (millis ());
+    Serial.print (" - conn: ");
+    Serial.print (++connection);
+    Serial.print (" - (");
+    Serial.print (ESP.getFreeHeap ());
+    Serial.println (")");
+
+   int rc = ipstack.connect(aws_endpoint, port);
+    if (rc != 1)
+    {
+      Serial.println("error connection to the websocket server");
+      return false;
+    } else {
+      Serial.println("websocket layer connected");
+    }
+
+    Serial.println("MQTT connecting");
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 4;
+    char* clientID = generateClientID ();
+    data.clientID.cstring = clientID;
+    rc = client.connect(data);
+    delete[] clientID;
+    if (rc != 0)
+    {
+      Serial.print("error connection to MQTT server");
+      Serial.println(rc);
+      return false;
+    }
+    Serial.println("MQTT connected");
+    return true;
 }
 
 //count messages arrived
@@ -97,75 +219,29 @@ void messageArrived(MQTT::MessageData& md)
   Serial.print("Payload ");
   char* msg = new char[message.payloadlen+1]();
   memcpy (msg,message.payload,message.payloadlen);
-  Serial.println(msg);
+  Serial.print("[");
+  Serial.print(msg);
+  Serial.println("]");
+  pos = atoi(msg);
+  servo_move(pos);
   delete msg;
-}
-
-//connects to websocket layer and mqtt layer
-bool connect () {
-
-
-
-    if (client.isConnected ()) {    
-        client.disconnect ();
-    }  
-    //delay is not necessary... it just help us to get a "trustful" heap space value
-    delay (1000);
-    Serial.print (millis ());
-    Serial.print (" - conn: ");
-    Serial.print (++connection);
-    Serial.print (" - (");
-    Serial.print (ESP.getFreeHeap ());
-    Serial.println (")");
-
-
-
-
-   int rc = ipstack.connect(aws_endpoint, port);
-    if (rc != 1)
-    {
-      Serial.println("error connection to the websocket server");
-      return false;
-    } else {
-      Serial.println("websocket layer connected");
-    }
-
-
-    Serial.println("MQTT connecting");
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-    data.MQTTVersion = 4;
-    char* clientID = generateClientID ();
-    data.clientID.cstring = clientID;
-    rc = client.connect(data);
-    delete[] clientID;
-    if (rc != 0)
-    {
-      Serial.print("error connection to MQTT server");
-      Serial.println(rc);
-      return false;
-    }
-    Serial.println("MQTT connected");
-    return true;
 }
 
 //subscribe to a mqtt topic
 void subscribe () {
-   //subscript to a topic
-    int rc = client.subscribe(aws_topic, MQTT::QOS0, messageArrived);
-    if (rc != 0) {
+   int rc = client.subscribe(aws_topic_2, MQTT::QOS1, messageArrived);
+   if (rc != 0) {
       Serial.print("rc from MQTT subscribe is ");
       Serial.println(rc);
       return;
-    }
-    Serial.println("MQTT subscribed");
+   }
+   Serial.println("MQTT subscribed");
 }
 
 //send a message to a mqtt topic
 void sendmessage (double humidity, double tempC) {
     time_t t = time(NULL);
     unsigned long seconds = (unsigned long) t;
-    Serial.println(seconds);
-
     StaticJsonBuffer<200> jsonBuffer;
     JsonObject& root = jsonBuffer.createObject();
     root["id"] = "id" + String(seconds);
@@ -178,6 +254,7 @@ void sendmessage (double humidity, double tempC) {
   
     //send a message
     MQTT::Message message;
+    //strcpy(buf, "{\"state\":{\"reported\":{\"on\": false}, \"desired\":{\"on\": false}}}");
     message.qos = MQTT::QOS0;
     message.retained = false;
     message.dup = false;
@@ -186,38 +263,10 @@ void sendmessage (double humidity, double tempC) {
     int rc = client.publish(aws_topic, message); 
 }
 
-
-void setup() {
-    wifi_set_sleep_type(NONE_SLEEP_T);
-    Serial.begin (115200);
-    delay (2000);
-    Serial.setDebugOutput(1);
-
-    //fill with ssid and wifi password
-    WiFiMulti.addAP(wifi_ssid, wifi_password);
-    Serial.println ("connecting to wifi");
-    while(WiFiMulti.run() != WL_CONNECTED) {
-        delay(100);
-        Serial.print (".");
-    }
-    Serial.println ("\nconnected");
-
-    //fill AWS parameters    
-    awsWSclient.setAWSRegion(aws_region);
-    awsWSclient.setAWSDomain(aws_endpoint);
-    awsWSclient.setAWSKeyID(aws_key);
-    awsWSclient.setAWSSecretKey(aws_secret);
-    awsWSclient.setUseSSL(true);
-
-    if (connect ()){
-      //subscribe ();
-    }
-}
-
 void send_am(double humidity, double tempC, double tempF, double heatIndexC, double heatIndexF) {
   char humidbuf[12];
-  Serial.print("ambient ...");
-
+  Serial.println("ambient...");
+  
   ambient.begin(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY, &wifi);
   ambient.set(1, tempC);
   dtostrf(humidity, 3, 1, humidbuf);
@@ -227,68 +276,35 @@ void send_am(double humidity, double tempC, double tempF, double heatIndexC, dou
   Serial.println("... sended.");
 }
 
-void do_sleep(){
-  Serial.println("do sleep...");
-  ESP.deepSleep(60 * 5 * 1000 * 1000);  
+void servo_move(int p){
+  Serial.print("angle: ");
+  Serial.println(p);
+
+  // set the servo position
+  kameServo.writeMicroseconds(p);
+  delay(1000);
 }
 
 void loop() {
-  //keep the mqtt up and running
-  if (awsWSclient.connected ()) {    
+  int sleepTime = 1000 * 60 * 5; //5 minutes
+
+  while(sleepTime > 0) {  
+    sleepTime -= 1000 * 15;
+    Serial.println(".");
+
+    //keep the mqtt up and running
+    if (awsWSclient.connected ()) {    
       client.yield(50);
-  } else {
-    //handle reconnection
-    if (connect ()){
-      //subscribe ();      
+    } else {
+      //handle reconnection
+      if (connect ()){
+        subscribe ();      
+      }
     }
+    
+    delay(1000 * 15);
   }
 
-  int timeSinceLastRead = 10;
-  float h;
-  float t;
-  float f;
-  while(timeSinceLastRead > 0) {
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
-    t = dht.readTemperature();
-    // Read temperature as Fahrenheit (isFahrenheit = true)
-    f = dht.readTemperature(true);
-    if(isnan(h) || isnan(t)) {
-      Serial.println("Failed to read from DHT sensor!");
-      timeSinceLastRead -= 1;
-      delay(500);
-    }else{
-      timeSinceLastRead = 0;
-    }
-  }
-
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
-
-  send_am(h, t, f, hic, hif);
-  delay (100);
-  sendmessage (h,t);
-  Serial.println("Reported!: ");
-
-  delay (1000);
-  do_sleep();
-  delay (1000);
+  //ESP.deepSleep(sleepTime * 1000);
+  //ESP.restart();
 }
